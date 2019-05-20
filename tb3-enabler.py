f4e2bd12e0cfdc1270a5a11144239e8e3c196f2c
feijisu27 <feijisu27@gmail.com>
2019-05-20 23:38:40 +0800

    could you approve this fix for 10.14.5 (#25)
    * - fix the 10.14.2 issue.
    - uncomment the old key.
    
    * fix 10.14.5 issue.

--- tb3-enabler.py
+++ tb3-enabler.py
@@ -36,7 +36,8 @@ md5_version = {
     "f2ed59d381f83c61ad0ec835364b2b79": ["10.13.4 (17E199)"],
     "e64e2678bb38f6f60d3a9647c5d53eb6": ["10.13.4 (17E202)"],
     "f46b4c2d8025c479d9a66e62cfa6d851": ["10.14 (18A391)"],
-    "8de0163c0ae5dab4b725dab2d9a1f0a1": ["10.14.2 (18C54)"]
+    "8de0163c0ae5dab4b725dab2d9a1f0a1": ["10.14.2 (18C54)"],
+    "17ab6da0bd11078a35c88a1380bc378e": ["10.14.5 (18F132)"]
 }
 md5_patch = {
     "00e2f0eb5db157462a83e4de50583e33": "a6c2143c2f085c2c104369d7a1adfe03",
@@ -55,7 +56,8 @@ md5_patch = {
     "f2ed59d381f83c61ad0ec835364b2b79": "9591b6f618d5d6ecc016488bc29e2b9f",
     "e64e2678bb38f6f60d3a9647c5d53eb6": "555a6efce42709e06530251eb0f71315",
     "f46b4c2d8025c479d9a66e62cfa6d851": "07a13e6e367608846ecbe3a1154bd05c",
-    "8de0163c0ae5dab4b725dab2d9a1f0a1": "b242fcabdf46bb83f083af2c97179bb2"
+    "8de0163c0ae5dab4b725dab2d9a1f0a1": "b242fcabdf46bb83f083af2c97179bb2",
+    "17ab6da0bd11078a35c88a1380bc378e": "c49dccab2d8b23b3f170efc3e5519941"
 }
 md5_patch_r = dict((v, k) for k, v in md5_patch.items())
 
